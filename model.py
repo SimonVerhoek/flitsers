de0558e665af87db478fec9bb08cb55eb36616f9
Simon <saverhoek@gmail.com>
2016-03-06 14:31:42 +0100

    serialization now makes use of marshmallow library

--- model.py
+++ model.py
@@ -1,36 +1,48 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, Date, Time, Float
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from flask.ext.jsontools import JsonSerializableBase
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema, fields
 
-# from credentials import SQLALCHEMY_DATABASE_URI
-
-
-Base = declarative_base(cls=(JsonSerializableBase,))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/flitsers'
+db = SQLAlchemy(app)
 
 
-class Melding(Base):
+class Melding(db.Model):
 	__tablename__ = 'melding'
-	id = Column(Integer, primary_key=True, nullable=False)
-	datum = Column(Date, nullable=False, default=datetime.now())
-	soort_weg = Column(String, nullable=False)
-	wegnummer = Column(String, nullable=False)
-	zijde = Column(String, nullable=False)
-	hm_paal = Column(String, nullable=False)
-	type_controle = Column(String, nullable=False)
-	tijd_van_melden = Column(Time, nullable=False)
-	details = Column(String)
-	laatste_activiteit = Column(Time)
-	locatie_lat = Column(Float)
-	locatie_lon = Column(Float)
+	id = db.Column(db.Integer, primary_key=True, nullable=False)
+	datum = db.Column(db.DateTime, nullable=False, default=datetime.now())
+	soort_weg = db.Column(db.String, nullable=False)
+	wegnummer = db.Column(db.String, nullable=False)
+	zijde = db.Column(db.String, nullable=False)
+	hm_paal = db.Column(db.String, nullable=False)
+	type_controle = db.Column(db.String, nullable=False)
+	tijd_van_melden = db.Column(db.Time, nullable=False)
+	details = db.Column(db.String)
+	laatste_activiteit = db.Column(db.DateTime)
+	locatie = db.Column(db.String)
+	locatie_lat = db.Column(db.Float)
+	locatie_lon = db.Column(db.Float)
 
 
-engine = create_engine(DATABASE_URL)
+# JSON-serializable version
+class MeldingSchema(Schema):
+	id = fields.Integer()
+	datum = fields.Date()
+	soort_weg = fields.String()
+	wegnummer = fields.String()
+	zijde = fields.String()
+	hm_paal = fields.String()
+	type_controle = fields.String()
+	tijd_van_melden = fields.Time()
+	details = fields.String()
+	laatste_activiteit = fields.DateTime()
+	locatie = fields.String()
+	locatie_lat = fields.Float()
+	locatie_lon = fields.Float()
+
 
 # create db with above classes as tables
 #Base.metadata.create_all(engine)
 
-db_session = sessionmaker(bind=engine)
--- routes.py
+++ routes.py
@@ -1,43 +1,22 @@
-from flask import Flask, render_template
-
 import datetime
 from json import dumps
 
-from model import db_session
-from model import Melding
- 
-app = Flask(__name__)      
- 
+from flask import render_template
+from flask.ext.heroku import Heroku
+
+from model import *
+
+melding_schema = MeldingSchema()
+meldingen_schema = MeldingSchema(many=True)
+
+
 @app.route('/')
 def home():
-	s = db_session()
-	data = s.query(Melding).all()
-
-	flitsers = []
-	
-	for flitser in data:
-		s.expunge(flitser)
-
-		# format dates and times for jsonification
-		flitser.datum = flitser.datum.isoformat()
-		flitser.tijd_van_melden = flitser.tijd_van_melden.isoformat()
-		if flitser.laatste_activiteit:
-			flitser.laatste_activiteit = flitser.laatste_activiteit.isoformat()
-
-		flitser = flitser.__json__()
-
-		flitsers.append(flitser)
-
+	data = Melding.query.all()
+	flitsers = meldingen_schema.dump(data)
 	return render_template('home.html', flitsers=flitsers)
-
-
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
-    if isinstance(obj, datetime.date):
-        serial = obj.isoformat()
-        return serial
-    raise TypeError ("Type not serializable")
 
  
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.debug = True
+	app.run()
