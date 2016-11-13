12c8966cf4ea4d2b3e320424e6c18ff56726e1fe
Simon <saverhoek@gmail.com>
2016-11-13 16:29:40 +0100

    updated scraper bash script

--- scraper.sh
+++ scraper.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 set -e
 
+# local development
+# echo 'Starting Tor...'
+# tor > /dev/null &
+# sleep 10
+
+# echo 'Starting scraper...'
+# python scraper.py
+
+# echo 'Killing Tor...'
+# kill $(lsof -t -i:9050)
+
+
+
+# production
 echo 'Starting Tor...'
-tor > /dev/null &
+sudo service tor start
 sleep 10
 
 echo 'Starting scraper...'
 python scraper.py
 
 echo 'Killing Tor...'
-kill $(lsof -t -i:9050)
+sudo service tor stop
