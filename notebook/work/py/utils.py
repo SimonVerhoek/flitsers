1c4eff8bb015e5c8d73922b1b190b1854b605c92
Simon Verhoek <simon@simonic.nl>
2023-10-03 12:15:45 +0200

    fixed encoding stage by implementing Dask

--- notebook/work/py/utils.py
+++ notebook/work/py/utils.py
@@ -1,8 +1,10 @@
+from typing import Type
+
 import pandas as pd
 
 
-def one_hot_encode(df: pd.DataFrame, column: str) -> pd.DataFrame:
-    one_hot = pd.get_dummies(df[column], prefix=column)
+def one_hot_encode(df: pd.DataFrame, column: str, dtype: Type[bool | int | float] = bool) -> pd.DataFrame:
+    one_hot = pd.get_dummies(df[column], prefix=column, dtype=dtype)
     df = df.drop(column, axis=1)
     df = df.join(one_hot)
     return df
