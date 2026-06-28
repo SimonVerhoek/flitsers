5afb4c1f6a198a3db2b87db7b43c6a00872dcb37
Simon Verhoek <simon@simonic.nl>
2026-06-28 19:55:03 +0200

    fixed scraper script

--- app/scraper.sh
+++ app/scraper.sh
@@ -2,21 +2,13 @@
 set -e
 source $HOME/.profile
 
-# local development
 echo 'Starting Tor...'
 tor > /dev/null &
 sleep 15
 
-# echo 'Starting scraper...'
+echo 'Starting scraper...'
 source venv/bin/activate
-python scraper.py
+python app/scraper.py
 
-# echo 'Killing Tor...'
+echo 'Killing Tor...'
 kill "`pidof tor`"
-
-
-
-# production
-#echo 'Starting scraper...'
-#source ~/.virtualenvs/flitserdata/bin/activate
-#python3.7 ~/flitsers/scraper.py
